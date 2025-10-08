a613ee8d93dc6e9fec46d1d513e86fcf9d016dc9
Nam Vo <nam.vh@wearecare.sg>
2025-10-09 02:26:05 +0700

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 import starlightThemeNova from "starlight-theme-nova";
 
 export default defineConfig({
-  site: "https://vhnam.github.io",
+  site: "https://vhnam.github.io/vibe-planner-docs",
   redirects: {
     "/": {
       status: 302,
